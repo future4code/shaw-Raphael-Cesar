3421a1241a80f692a896189f40917390e5974729
unknown <raphapcvale@gmail.com>
2022-03-26 18:03:02 -0300

    projeto-whatslab

---
+++ modulo2/projeto-whatslab/chat/src/App.js
@@ -0,0 +1,54 @@
+import React from "react";
+import "./App.css";
+import styled from "styled-components"
+import {Chat} from "./Chat";
+
+const AppContainer = styled.div`
+  max-width: 600px;
+  height: 100vh;
+  border: 1px solid black;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+`
+
+const MessagesContainer = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  padding: 20px;
+`
+
+const BoldText = styled.span`
+  font-weight: bold;
+`
+
+class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      messages: []
+    }
+  }
+
+  addMessage = (message) => {
+    this.setState({ messages: [...this.state.messages, message] })
+  }
+
+  render() {
+    return (
+      <AppContainer>
+        <MessagesContainer>
+          {
+            this.state.messages.map((message, index) => <p key={index}><BoldText>{message.user}</BoldText>{': ' + message.text}</p>)
+          }
+        </MessagesContainer>
+        <Chat addMessage={this.addMessage} />
+      </AppContainer>
+    );
+  }
+}
+
+export default App;
--- modulo2/projeto-whatslab/src/App.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import chat from './chat'
-
-const app = () => <chat> 
-  
-export default App;
