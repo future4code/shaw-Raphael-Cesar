595704a87cfcf1a191d25a7ff9855af0aa350dd3
unknown <raphapcvale@gmail.com>
2022-06-01 17:47:19 -0300

    fluxo-de-dados

--- modulo4/fluxo-de-dados-backend/src/index.ts
+++ modulo4/fluxo-de-dados-backend/src/index.ts
@@ -60,3 +60,98 @@ app.post('/produto', (req:Request, res:Response)=>{
 
 
 
+//4
+
+app.get('/produto', (req:Request, res:Response)=>{
+    res.status(200).send({message: 'Lista de produtos', produtos})
+})
+
+
+
+//5
+
+app.put('/produto', (req:Request, res:Response)=>{
+try{
+    const id = req.query.id
+    const {price} = req.body
+
+    if(!price){
+        throw new Error('Campo não preenchido')
+    }
+    
+    if(typeof price !== 'number'){
+        throw new Error('Preço tem que ser um number')
+    }
+    if( price <= 0 ){
+        throw new Error('Preço tem que ser maior que 0')
+    }
+
+    let Found = false
+   
+
+    for(let i = 0; i < produtos.length; i++){
+        if(produtos[i].id === id){
+            produtos[i].price = price
+            Found = true
+        }
+    }
+    if(!Found){
+        throw new Error('Produto não encontrado')
+    }
+    res.status(200).send({message: 'Deu certo!', produtos})
+}
+catch(error: any){
+    switch(error.message){
+        case 'Campo não preenchido':
+            res.statusCode = 422
+            break
+        case 'Preço tem que ser um number':
+            res.status(409)
+            break
+        case 'Preço tem que ser maior que 0':
+            res.status(409)
+            break
+        case 'Produto não encontrado':
+            res.status(422)
+            break
+        default:
+            res.status(500)
+    }
+    res.send(error.message)
+}
+})
+
+
+
+//6
+
+app.delete('/produto', (req:Request, res:Response)=>{
+try{
+    const id = req.query.id
+
+    let Found = false
+    for(let i = 0; i < produtos.length; i++){
+        if(produtos[i].id === id){
+            produtos.splice(i, 1)
+            Found = true
+        }
+    }
+
+    if(!Found){
+        throw new Error('Produto não encontrado')
+    }
+
+    res.status(200).send({message: 'Deu certo!', produtos})
+}
+
+    catch(error: any){
+        switch(error.message){
+            case 'Produto não encontrado':
+                res.status(422)
+                break
+            default:
+                res.status( 500)
+        }
+        res.send(error.message)
+}
+})
